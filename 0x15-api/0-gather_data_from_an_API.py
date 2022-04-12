085d6629e29d6c94a13732b968365973e8a4298b
QuincyMashava <mashavaquincy@gmail.com>
2022-04-11 17:52:24 -0700

    corrections of output on checker

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,32 +3,33 @@
 Request from API; Return TODO list progress given employee ID
 """
 import requests
-from sys import argv
-
-
-def display():
-    """return API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in users.json():
-        if u.get('id') == int(argv[1]):
-            EMPLOYEE_NAME = (u.get('name'))
-            break
-    TOTAL_NUM_OF_TASKS = 0
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for t in todos.json():
-        if t.get('userId') == int(argv[1]):
-            TOTAL_NUM_OF_TASKS += 1
-            if t.get('completed') is True:
-                    NUMBER_OF_DONE_TASKS += 1
-                    TASK_TITLE.append(t.get('title'))
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
-                                                          NUMBER_OF_DONE_TASKS,
-                                                          TOTAL_NUM_OF_TASKS))
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
+import sys
 
 
 if __name__ == "__main__":
-    display()
+
+    number_completed_todos = 0
+    number_of_todos = 0
+    completed_tasks = []
+    users_url = 'https://jsonplaceholder.typicode.com/users/'
+    user_url = users_url + sys.argv[1]
+    user_infos = requests.get(user_url).json()
+
+    user_name = user_infos.get("name")
+
+    user_todos_url = users_url + sys.argv[1] + '/todos/'
+    user_todos = requests.get(user_todos_url).json()
+
+    for user_todo in user_todos:
+        number_of_todos += 1
+        if (user_todo.get("completed") is True):
+            number_completed_todos += 1
+            completed_tasks.append(user_todo.get("title"))
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name,
+        number_completed_todos,
+        number_of_todos
+        ))
+    for task in completed_tasks:
+        print("\t {}".format(task))
